b63e42cbc91929fa7bc4e72668ba4c9722a92a73
Tsukune <tsukuneaune@gmail.com>
2020-03-16 11:16:08 +0700

    add lab 1

--- lab1/lab1/Form1.cs
+++ lab1/lab1/Form1.cs
@@ -19,58 +19,73 @@ namespace lab1
         int factorial(int x)
         {
             if (x > 1)
-             x *= factorial(x - 1);
+                x *= factorial(x - 1);
 
             return x;
         }
-        double TheorPoisson(double p,int m )
+        double TheorPoisson(double p, int m)
         {
             double n = Math.Floor(m / p);
             double a = p * n;
-            double P = (Math.Pow(a, m)/factorial(m))*Math.Exp(-a);
-            return Math.Floor(P*n);
+            double P = (Math.Pow(a, m) / factorial(m)) * Math.Exp(-a);
+            return P;
         }
-        int EmpPoisson(int m,double P)
+        double EmpPoisson(int m, double P)
         {
+            int i;
             double n;
-            int count=0;
+            int count = 0;
             Random ran = new Random();
-            n = Math.Floor(m/ P);
-            for (int i = 0; i < n; i++)
+            n = Math.Floor(m / P);
+
+            for (i = 0; i < n; i++)
             {
                 double E = ran.NextDouble();
                 if (P > E)
-                   count++;
+                    count++;
             }
 
-            return count;
+            return (double)count / (i + 1);
         }
-       int ModPoisson(int m)
+        double ModPoisson(int m)
         {
             int k = 1;
             Random rand = new Random();
-            double randnumber=rand.NextDouble();
+            double randnumber = rand.NextDouble();
             if (randnumber < Math.Exp(-m))
-                return 0;  
+                return 1;
 
             while (randnumber > Math.Exp(-m))
             {
                 randnumber *= rand.NextDouble();
                 k++;
             }
-            return k;   
+            return (double)1 / k;
+        }
+
+        double CriterionPearson(double v1, double v2)
+        {
+            double xi = Math.Pow((v1 - v2 * 0.5f), 2) / v2 * 0.5f;
+            return xi;
+        }
+        bool Compare(double xi1, double xi2)
+        {
+            return xi1 < xi2;
         }
         private void Button1_Click(object sender, EventArgs e)
         {
             Random ran = new Random();
             double P = 0.1 + ran.NextDouble() % 0.1;
-            textBox1.Text= "EmpPoison number ="+EmpPoisson(3,P).ToString()+'\r'+'\n';
-            textBox1.Text += "TheorPoison number=" + TheorPoisson(P, 3) + '\r' + '\n';
-            textBox1.Text += "ModPoison number=" + ModPoisson(3) ;
-
+            double emp = EmpPoisson(3, P);
+            double theor = TheorPoisson(P, 3);
+            double mod = ModPoisson(3);
+            textBox1.Text = "EmpPoison number =" + emp + '\r' + '\n';
+            textBox1.Text += "TheorPoison number=" + theor + '\r' + '\n';
+            textBox1.Text += "ModPoison number=" + mod + '\r' + '\n';
+            textBox1.Text += "Poison > ModPoison? " + Compare(CriterionPearson(emp, theor), CriterionPearson(mod, theor));
         }
 
-        
+
     }
- 
+
 }
