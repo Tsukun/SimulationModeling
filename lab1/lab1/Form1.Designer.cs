888e9ad0476cc0aa4c0656e9f2b89e952e6d4604
Tsukune <tsukuneaune@gmail.com>
2020-03-16 15:18:33 +0700

    add gist

--- lab1/lab1/Form1.Designer.cs
+++ lab1/lab1/Form1.Designer.cs
@@ -28,13 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(571, 122);
+            this.button1.Location = new System.Drawing.Point(267, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 33);
             this.button1.TabIndex = 0;
@@ -44,21 +60,115 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 56);
+            this.textBox1.Location = new System.Drawing.Point(31, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(209, 112);
             this.textBox1.TabIndex = 2;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(46, 211);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(470, 211);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(267, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "математическое ожидание";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "метод Пуассона";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "модифицированный метод Пуассона";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(267, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "число интервалов";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +178,14 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- lab1/lab1/Form1.cs
+++ lab1/lab1/Form1.cs
@@ -12,6 +12,7 @@ namespace lab1
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -20,37 +21,57 @@ namespace lab1
         {
             if (x > 1)
                 x *= factorial(x - 1);
+            
+            if (x == 0)
+                return 1;
 
             return x;
         }
-        double TheorPoisson(double p, int m)
+        double TheorPoisson(double p, double mx)
         {
-            double n = Math.Floor(m / p);
-            double a = p * n;
-            double P = (Math.Pow(a, m) / factorial(m)) * Math.Exp(-a);
+            double n = Math.Floor(mx / p);
+            int m =(int)Math.Floor(p * n);
+            double P = (Math.Pow(mx, m) / factorial(m)) * Math.Exp(-mx);
             return P;
         }
-        double EmpPoisson(int m, double P)
+        double EmpPoisson(double m, double P)
         {
             int i;
-            double n;
+            int n;
+            int countinter= Convert.ToInt32(textBox3.Text);
+            double[] interval = new double[countinter];
+            int[] temp = new int[countinter];
+            interval[0] = P/ countinter;
+            for (int j = 1; j < countinter; j++)
+                interval[j] = interval[0] + interval[0] * j;
             int count = 0;
-            Random ran = new Random();
-            n = Math.Floor(m / P);
-
+            n = (int)Math.Floor(m / P);
+            double[] arr = new double[n];
             for (i = 0; i < n; i++)
             {
-                double E = ran.NextDouble();
+                double E = rand.NextDouble();
                 if (P > E)
+                {
                     count++;
+                    arr[i] = E;
+                }
+                
             }
+            for (int k = 0; k < countinter; k++)
+                for (int j = 0; j < n; j++)
+                    if (arr[j] < interval[k])
+                    {
+                        temp[k]++;
+                        break;
+                    }
 
+            for (int j = 0; j < countinter; j++)
+                chart2.Series[0].Points.AddXY(interval[j], temp[j]);
             return (double)count / (i + 1);
         }
-        double ModPoisson(int m)
+        double ModPoisson(double m)
         {
             int k = 1;
-            Random rand = new Random();
             double randnumber = rand.NextDouble();
             if (randnumber < Math.Exp(-m))
                 return 1;
@@ -59,13 +80,14 @@ namespace lab1
             {
                 randnumber *= rand.NextDouble();
                 k++;
+                chart1.Series[0].Points.AddXY(k, randnumber);
             }
             return (double)1 / k;
         }
 
         double CriterionPearson(double v1, double v2)
         {
-            double xi = Math.Pow((v1 - v2 * 0.5f), 2) / v2 * 0.5f;
+            double xi = Math.Pow((v1 - v2 * 0.5f), 2) / (v2 * 0.5f);
             return xi;
         }
         bool Compare(double xi1, double xi2)
@@ -74,18 +96,21 @@ namespace lab1
         }
         private void Button1_Click(object sender, EventArgs e)
         {
-            Random ran = new Random();
-            double P = 0.1 + ran.NextDouble() % 0.1;
-            double emp = EmpPoisson(3, P);
-            double theor = TheorPoisson(P, 3);
-            double mod = ModPoisson(3);
-            textBox1.Text = "EmpPoison number =" + emp + '\r' + '\n';
-            textBox1.Text += "TheorPoison number=" + theor + '\r' + '\n';
-            textBox1.Text += "ModPoison number=" + mod + '\r' + '\n';
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            double mx = Convert.ToInt32(textBox2.Text);
+            //Random ran = new Random();
+            double P = 0.1 + rand.NextDouble() % 0.1;
+            double emp = EmpPoisson(mx, P);
+            double theor = TheorPoisson(P, mx);
+            double mod = ModPoisson(mx);
+            textBox1.Text = "EmpPoison frequency =" + emp + '\r' + '\n';
+            textBox1.Text += "TheorPoison frequency=" + theor + '\r' + '\n';
+            textBox1.Text += "ModPoison frequency=" + mod + '\r' + '\n';
             textBox1.Text += "Poison > ModPoison? " + Compare(CriterionPearson(emp, theor), CriterionPearson(mod, theor));
         }
 
-
+     
     }
 
 }
